f1b52a237672064cc5221a6c5afb9d5188cc2814
lucifer7 <skyvoice@foxmail.com>
2016-05-06 17:59:59 +0800

    temp modify

---
+++ shiro-redis-session-webapp/src/test/InsertTest.java
@@ -0,0 +1,14 @@
+package redis.test;
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+
+public class InsertTest {
+    @Autowired
+    private ShiroSessionService shiroSessionService;
+
+    @Test
+    public void insert() {
+
+    }
+}
